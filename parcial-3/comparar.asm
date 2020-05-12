8807eb6645cbc88634a60f4ffa8ebba0bc8f8b78
ermiry <erick_salas@ermiry.com>
2020-05-12 13:38:03 -0500

    Refactor comparar.asm

--- parcial-3/comparar.asm
+++ parcial-3/comparar.asm
@@ -1,15 +1,15 @@
 #include "p16F628a.inc"    ;incluir librerias relacionadas con el dispositivo
  __CONFIG _FOSC_INTOSCCLK & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _CP_OFF    
-;configuración del dispositivotodo en OFF y la frecuencia de oscilador
+;configuraci?n del dispositivotodo en OFF y la frecuencia de oscilador
 ;es la del "reloj del oscilador interno" (INTOSCCLK)    
 RES_VECT  CODE    0x0000            ; processor reset vector
     GOTO    START                   ; go to beginning of program
 ; TODO ADD INTERRUPTS HERE IF USED
 MAIN_PROG CODE                      ; let linker place main program
-;variables para el contador:
- i equ 0x30
- j equ 0x31
- k equ 0x32
+
+; declarar variable temporal
+A equ 0x30
+E equ 0X31
  
 ;inicio del programa:
 START
@@ -18,58 +18,70 @@ CLRF TRISB  ;puerto B salida
 BSF TRISA,1 ; pin 1 puerto A entrada
 BCF STATUS, RP0 ;Regresar al banco 0
  
-inicio  
-MOVLW b'00001001'
-MOVWF PORTB
- 
-proceso: BTFSS PORTA,1
-GOTO proceso
-CALL confirmacion
-goto bandera
- 
- 
-confirmacion: BTFSC PORTA,1
-GOTO confirmacion
-CALL restar
-    RETURN
-   
-restar:
-MOVLW b'00000001'
-SUBWF PORTB,1
- CLRW
- RETURN
-   
-bandera: MOVLW b'11111111'
-    SUBWF PORTB, 0
-    BTFSS STATUS, Z ;Z==1?
-    goto posoneg
-    call cero
-   ;Positivo c=1, z=0
-   ;Negativo c=0, z=0
-   ;Cero     c=1, z=1
-   
-posoneg: BTFSS STATUS, C ;c=1?
-   goto negativo
-   goto positivo
-negativo: BCF PORTB, 3
-    BCF PORTB, 2
-    BCF PORTB, 4
-    BSF PORTB, 3
-   goto restart
-positivo: BCF PORTB, 3
-    BCF PORTB, 2
-    BCF PORTB, 4
-    BSF PORTB, 2
-   goto proceso
-cero: BCF PORTB, 3
-   BCF PORTB, 2
-   BCF PORTB, 4
-   BSF PORTB, 4
-   goto restart
-   
-   
-restart: MOVLW b'00001001'
+
+inicio:
+    MOVLW b'00000000'
+    MOVWF A
+    
+    MOVLW b'00000000'
+    MOVWF E
+    
+    MOVLW b'00000000'
     MOVWF PORTB
-    GOTO proceso
+    
+    BSF A, 0
+    BSF A, 1
+;    BSF A, 2
+;    BSF A, 3
+;    BSF A, 4
+;    BSF A, 5
+;    BSF A, 6
+;    BSF A, 7
+    
+    BSF E, 0
+    BSF E, 1
+    BSF E, 2
+;    BSF E, 3
+;    BSF E, 4
+;    BSF E, 5
+;    BSF E, 6
+;    BSF E, 7
+    
+    ; restart A - E
+    MOVF E, 0
+    SUBWF A, 0
+    
+    BTFSS STATUS, Z
+    goto checa	    ; son diferentes
+    goto zero	    ; si son iguales - Z es 1
+    
+; checar si el resultado fue positivo o negativo
+checa:
+    BTFSS STATUS, C
+    goto neg
+    goto pos
+    NOP
    
-    END
+; el resultado es positivo, A es mas grande
+pos:
+    MOVWF PORTB	    ; resultado de la resta
+    MOVLW b'00000001'
+    MOVWF PORTB	    ; prender led
+    NOP
+    goto inicio
+    
+neg:
+    MOVWF PORTB	    ; resultado de la resta
+    MOVLW b'00000010'
+    MOVWF PORTB	    ; prender led
+    NOP
+    goto inicio
+
+zero:
+    MOVWF PORTB	    ; resultado de la resta
+    MOVLW b'00000100'
+    MOVWF PORTB	    ; prender led
+    NOP
+    goto inicio
+    
+END
